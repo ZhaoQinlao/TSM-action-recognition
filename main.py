140ee908ac774e8cafc7c0a51389a45aebdf7b92
ZhaoQinlao <ZhaoQinlao@gmail.com>
2025-04-27 19:57:12 +0800

    print semantic loss

--- main.py
+++ main.py
@@ -296,10 +296,12 @@ def train(train_loader, model, criterion, optimizer, epoch, log, tf_writer):
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Semantic Loss {semantic_loss:.4f}\t'
                       'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
                       'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
                 epoch, i, len(train_loader), batch_time=batch_time,
-                data_time=data_time, loss=losses, top1=top1, top5=top5, lr=optimizer.param_groups[-1]['lr'] * 0.1))  # TODO
+                data_time=data_time, loss=losses, semantic_loss=semantic_loss if args.semantic else 0,
+                top1=top1, top5=top5, lr=optimizer.param_groups[-1]['lr'] * 0.1))  # TODO
             # print(output)
             tqdm.write(output)
             log.write(output + '\n')
